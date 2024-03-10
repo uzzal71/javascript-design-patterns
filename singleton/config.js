7d6250ee766ca221adead58e2113d4b339ca974b
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-10 16:51:34 +0600

    javascript desing patterns

--- singleton/config.js
+++ singleton/config.js
@@ -1,3 +1,28 @@
-const config = ?
+const config = (function () {
+    let props = {}
+
+    let instance;
+
+    if(instance) {
+        return instance;
+    }
+
+    instance = {
+        get(prop) {
+            return props[prop];
+        }
+
+        set(props, value) {
+            props[prop] = value;
+        }
+
+        has(prop) {
+            const value = props[prop];
+            return value !== null && value !== undefined;
+        }
+    }
+
+    return instance;
+})();
 
 module.exports = config;
